9badc72062ebd6ea0134998c7a81ae0720f7c4ae
Onur Mehemmed <onurmamedov1@gmail.com>
2025-03-14 23:58:27 +0400

    first commit

---
+++ MyAspProject/Controllers/ComputerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspProject.Models;
+using System.Collections.Generic;
+
+namespace MyAspProject.Controllers
+{
+    public class ComputerController : Controller
+    {
+        // Simulating a database with a list
+        private static List<Computer> computers = new List<Computer>
+        {
+            new Computer { Id = 1, Brand = "Dell", Description = "High performance laptop" },
+            new Computer { Id = 2, Brand = "HP", Description = "Business-class laptop" },
+            new Computer { Id = 3, Brand = "Apple", Description = "MacBook Pro" }
+        };
+
+        // GET: /Computer/
+        public IActionResult Index()
+        {
+            return View(computers);
+        }
+
+        
+    }
+}
---
+++ MyAspProject/Models/Computer.cs
@@ -0,0 +1,11 @@
+﻿namespace MyAspProject.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+
+        
+    }
+}
